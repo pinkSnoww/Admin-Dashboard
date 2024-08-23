c2640ea9b3b02bc3593ebec357564f3e0efcad3d
Sheeva <shivaq1994@gmail.com>
2024-08-23 11:47:30 +0330

    ✨: Add Navigation Component

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,11 @@
 // /pages/index.tsx
 import Link from 'next/link';
 import { Menu } from '../components/Menu';
-
+import  topNavigation   from "../components/Navigation";
 const HomePage = () => {
   return (
     <div>
+      <topNavigation />
       <Menu isSettingAllowed={false} />
       <Link legacyBehavior href="/list">
         <a className="text-blue-500 hover:underline">Go to List</a>
